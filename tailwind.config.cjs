866ab906db27d2f599c7d7ab5c7b314aafa15f28
AndreSandblom <andre.sandblom@gmail.com>
2026-03-13 11:55:37 +0100

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
